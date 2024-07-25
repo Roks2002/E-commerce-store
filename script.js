5b89ddd136172dd803bb5297f43684e35c3e3f23
rok_1 <119943038+Roks2002@users.noreply.github.com>
2024-07-25 15:43:40 +0200

    Added pictures
    Added pictures

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ function handleScroll() {
   const arrow = document.querySelector(".arrow");
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > 50) {
-    // Adjust the 50px to when you want the arrow to hide
+  /* Arrow scroll hide */
+  if (scrollTop > 100) {
     arrow.classList.add("hidden");
   } else {
     arrow.classList.remove("hidden");
   }
 }
 
-// Add the scroll event listener
+// Scroll event listener za arrow
 window.addEventListener("scroll", handleScroll);
 
 // Handle arrow click
 document.getElementById("scroll-arrow").addEventListener("click", function () {
-  const featureSection = document.getElementById("feat-products");
+  const featureSection = document.getElementById("feat-prod");
   window.scrollTo({
-    top: featureSection.offsetTop + 100, // Adjust 100px to your desired extra scroll distance
+    top: featureSection.offsetTop + 100, // Adjust 100px for arrow height
     behavior: "smooth",
   });
 });
