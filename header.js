1aad32b8d3603b18e86edbdea80e372bbeea575d
rok_1 <119943038+Roks2002@users.noreply.github.com>
2024-07-30 19:58:43 +0200

    home page update
    Added footer, banner, brands, more products.

--- header.js
+++ header.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch((error) => console.error("Error loading header:", error));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      header.classList.add("hide");
+    } else {
+      // Scrolling up
+      header.classList.remove("hide");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+});
