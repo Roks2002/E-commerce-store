caec239dfeef02bdd38545fac728092b879a0eb7
rok_1 <119943038+Roks2002@users.noreply.github.com>
2024-08-06 14:29:26 +0200

    Updated
    updated

--- script.js
+++ script.js
@@ -21,3 +21,36 @@ document.getElementById("scroll-arrow").addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+document.getElementById("sort").addEventListener("change", function () {
+  const sortBy = this.value;
+  let items = Array.from(document.querySelectorAll(".pro"));
+  items.sort((a, b) => {
+    const aPrice = parseFloat(
+      a.querySelector(".price").innerText.replace("$", "")
+    );
+    e;
+    const bPrice = parseFloat(
+      b.querySelector(".price").innerText.replace("$", "")
+    );
+    const aName = a.querySelector(".name").innerText.toLowerCase();
+    const bName = b.querySelector(".name").innerText.toLowerCase();
+
+    switch (sortBy) {
+      case "price-asc":
+        return aPrice - bPrice;
+      case "price-desc":
+        return bPrice - aPrice;
+      case "name-asc":
+        return aName.localeCompare(bName);
+      case "name-desc":
+        return bName.localeCompare(aName);
+      default:
+        return 0;
+    }
+  });
+
+  const container = document.querySelector(".pro-container");
+  container.innerHTML = "";
+  items.forEach((item) => container.appendChild(item));
+});
